d8bf81a87bf55d492301c15b8cbaf636e45ba8b4
Bobbi Johnson <bjohnson@wlapcs.org>
2018-01-09 18:32:24 +0000

    second commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
-function setup{
-    
+function setup(){
+    createCanvas(600,400);
 }
 
-Function draw{
-    
+function draw(){
+    if(keyIsPressed){
+        blendMode(LIGHTEST);
+        if(key=="r"){
+            background(255,0,0);
+        }  
+        if(key=="g"){
+            background(0,255,0);
+        }
+        if(key=="b"){
+            background(0,0,255);
+        }
+        if(keyCode==32){
+            background(255,255,255);
+        }
+    }
+        
+    else{
+        background(50,50,50);
+    }
+}
+
+function keyReleased() {
+    blendMode(NORMAL);
 }
